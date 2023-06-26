7d2a0f40d1ee23ad14279f77b02d361e4f8c78ea
Jose Mauro <z.mauro@live.com>
2023-06-25 21:27:17 -0300

    Inicializa projeto com Maven

---
+++ src/main/java/com/accenture/gestaofornecimento/ApiGestaoFornecimentoApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.gestaofornecimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestaoFornecimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestaoFornecimentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/gestaofornecimento/ApiGestaoFornecimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.gestaofornecimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestaoFornecimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
