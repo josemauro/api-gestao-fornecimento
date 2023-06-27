fd3ddbc6a7a53509af07fd9da54faa35862989c6
Jose Mauro <z.mauro@live.com>
2023-06-26 22:43:02 -0300

    Add parametros faltantes e salva pessoa fisica quando passado na requisicao

--- src/main/java/com/accenture/gestaofornecimento/controller/FornecedorController.java
+++ src/main/java/com/accenture/gestaofornecimento/controller/FornecedorController.java
@@ -8,7 +8,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.accenture.gestaofornecimento.model.Fornecedor;
+import com.accenture.gestaofornecimento.model.PessoaFisica;
 import com.accenture.gestaofornecimento.repository.FornecedorRepository;
+import com.accenture.gestaofornecimento.repository.PessoaFisicaRepository;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @RestController
 @RequestMapping("/fornecedores")
@@ -17,9 +25,13 @@ public class FornecedorController {
 	@Autowired
 	private FornecedorRepository fornecedorRepository;
 	
+	@Autowired
+	private PessoaFisicaRepository pessoaRepository;
+	
 	@PostMapping(path="/salvar")
-	public @ResponseBody String criarFornecedor(@RequestParam String id, @RequestParam String nome,
-												@RequestParam String email) {
+	public @ResponseBody String criarFornecedor(@RequestParam String id, @RequestParam String nome, @RequestParam String email,
+												@RequestParam(required = false) String rg, 
+												@RequestParam(required = false) String dataNascimento) {
 		// @ResponseBody String retornada é própria response e não o nome de uma view
 		// @RequestParam parâmetro da requisição
 		if (fornecedorRepository.findById(id).isPresent()) {
@@ -33,25 +45,55 @@ public class FornecedorController {
 		fornecedor.setEmail(email);
 		
 		fornecedorRepository.save(fornecedor);
+
+		if (rg != null && dataNascimento!= null) {
+			PessoaFisica pessoa = new PessoaFisica();
+			pessoa.setIdFornecedor(id);
+			pessoa.setRg(rg);
+			Date data = new Date(1993, 05, 16);
+			pessoa.setDataNascimento(data);
+			
+			pessoaRepository.save(pessoa);
+		}
 		
 		return "Salvo com sucesso!";
 	  }
 	
 
 	@PostMapping(path="/atualizar")
-	public @ResponseBody String atualizarFornecedor(@RequestParam String id, @RequestParam String nome,
-													@RequestParam String email) {
+	public @ResponseBody String atualizarFornecedor(@RequestParam(required = false) String id, @RequestParam String nome,
+													@RequestParam(required = false) String email,
+													@RequestParam(required = false) String rg, 
+													@RequestParam(required = false) String dataNascimento) {
 
+		
 		if (!fornecedorRepository.findById(id).isPresent()) {
 			return "Erro ao atualizar, fornecedor não encontrado!";
 		}
-		Fornecedor fornecedor = new Fornecedor();
-		fornecedor.setId(id);
-		fornecedor.setNome(nome);
-		fornecedor.setEmail(email);
-		
+
+		Fornecedor fornecedor = fornecedorRepository.findById(id).get();
+
+		if (nome != null) {
+			fornecedor.setNome(nome);
+		}
+
+		if (email != null) {
+			fornecedor.setEmail(email);
+		}
+
+		PessoaFisica pessoa = pessoaRepository.findByIdFornecedor(id);
+
+		if (rg != null) {
+			pessoa.setRg(rg);			
+		}	
+		if (dataNascimento!= null) {
+			Date data = new Date(1993, 05, 16);
+			pessoa.setDataNascimento(data);
+		}
+
 		fornecedorRepository.save(fornecedor);
-		
+		pessoaRepository.save(pessoa);
+
 		return "Fornecedor atualizado com sucesso!";
 	  }
 
@@ -68,9 +110,12 @@ public class FornecedorController {
 	
 		Fornecedor fornecedor = new Fornecedor();
 		fornecedor.setId(id);
-		
+
+		PessoaFisica pessoa = pessoaRepository.findByIdFornecedor(id);
+
+		pessoaRepository.delete(pessoa);
 		fornecedorRepository.delete(fornecedor);
-		
+
 		return "Fornecedor deletado com sucesso!";
 	  }
 	
